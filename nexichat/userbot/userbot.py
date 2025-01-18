9bbc77aeb5efcc2239768006bc6cfaa85b4d7942
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-01-18 15:55:40 +0530

    Update userbot.py

--- nexichat/userbot/userbot.py
+++ nexichat/userbot/userbot.py
@@ -24,8 +24,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ll_KINGDOM_ll")
-                await self.one.join_chat("ll_IMPERIAL_ll")
+                await self.one.join_chat("Exampurrs")
+                await self.one.join_chat("FONT_CHANNEL_01")
                
 
             except:
