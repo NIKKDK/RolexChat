ea98c9a96efc6ac4470e13729b8e1d76f7713042
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-01-18 15:10:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,11 +12,11 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", "7745014754"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/amritraj78/KING-CHATBOT")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/masoombalak88/SYSTEM-CHATBOT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "ll_KINGDOM_ll"
-UPDATE_CHNL = "ll_IMPERIAL_ll"
-OWNER_USERNAME = "ll_BRANDED_ll"
+SUPPORT_GRP = "Examprrs"
+UPDATE_CHNL = "FONT_CHANNEL_01"
+OWNER_USERNAME = ""
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
