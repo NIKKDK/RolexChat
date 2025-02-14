2214d56a9f272c68d7fdd919c5d3442ab459a30e
NIKKDK <pikap5468@gmail.com>
2025-02-14 18:13:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7745014754"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/masoombalak88/SYSTEM-CHATBOT")
+OWNER_ID = int(getenv("OWNER_ID", "7338182362"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/NIKKDK/RolexChat")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "Exampurrs"
-UPDATE_CHNL = "FONT_CHANNEL_01"
+SUPPORT_GRP = "love_in_timeeee"
+UPDATE_CHNL = "love_in_timeee"
 OWNER_USERNAME = ""
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
